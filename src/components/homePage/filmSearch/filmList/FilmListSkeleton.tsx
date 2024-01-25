3172a8274c1988ae534c4a0592afdf0ec0650e5a
Alex Mykhailiuk <alexmykhailiuk@MacBook-Pro.local>
2024-01-26 00:11:14 +0200

    renamed ui to normal

--- src/components/homePage/filmSearch/filmList/FilmListSkeleton.tsx
+++ src/components/homePage/filmSearch/filmList/FilmListSkeleton.tsx
@@ -1,4 +1,4 @@
-import Skeleton from "../../../ui_1/Skeleton/Skeleton";
+import Skeleton from "../../../ui/Skeleton/Skeleton";
 import styles from "./filmList.module.scss";
 
 const FilmListSkeleton = () => {
