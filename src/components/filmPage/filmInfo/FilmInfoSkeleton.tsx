14103ae0d54195d876bca99c19ac2a9f52f820f6
Alex Mykhailiuk <alexmykhailiuk@MacBook-Pro.local>
2024-01-26 00:10:24 +0200

    temporary renamed

--- src/components/filmPage/filmInfo/FilmInfoSkeleton.tsx
+++ src/components/filmPage/filmInfo/FilmInfoSkeleton.tsx
@@ -1,5 +1,5 @@
-import SectionLine from "../../ui/SectionLine/SectionLine";
-import Skeleton from "../../ui/Skeleton/Skeleton";
+import SectionLine from "../../ui_1/SectionLine/SectionLine";
+import Skeleton from "../../ui_1/Skeleton/Skeleton";
 import styles from "./filmInfo.module.scss";
 import LoadingIcon from "../../../assets/icon/LoadingIcon";
 
