d46f49c3368acb289bf2c7ba6564fd84d8ac4871
Yoland172 <148059735+Yoland172@users.noreply.github.com>
2024-02-12 18:49:10 +0200

    Intv2 (#11)
    * aded internationalization and test gitHub action

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,47 +4,61 @@ import FacebookIcon from "../ui/icon/socialMedia/FacebookIcon";
 import InstagramIcon from "../ui/icon/socialMedia/InstagramIcon";
 import TwitterIcon from "../ui/icon/socialMedia/TwitterIcon";
 import TelegramIcon from "../ui/icon/socialMedia/TelegramIcon";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t, i18n } = useTranslation();
+  const languages = ["ua", "en","es"];
+
   return (
     <div className={styles.footerContainer}>
       <img src={transitionImg} alt="transitionImage" />
       <div className={styles.mainContainer}>
-        <div className={styles.bigText}>
-          <h1>ENJOY IT!</h1>
+        <div className={styles.infoContainer}>
+          <div className={styles.bigText}>
+            <h1>{t("footer.enjoyIt")}</h1>
+          </div>
+          <div className={styles.titlesContainer}>
+            <div className={styles.section}>
+              <h1>{t("footer.columnArticle.legal")}</h1>
+              <p>{t("footer.columnAddInfo.terms")}</p>
+              <p>{t("footer.columnAddInfo.privacy")}</p>
+            </div>
+            <div className={styles.section}>
+              <h1>{t("footer.columnArticle.explore")}</h1>
+              <p>{t("footer.columnAddInfo.home")}</p>
+              <p>{t("footer.columnAddInfo.about")}</p>
+              <p>{t("footer.columnAddInfo.careers")}</p>
+            </div>
+            <div className={styles.section}>
+              <h1>{t("footer.columnArticle.getInTouch")}</h1>
+              <h3>workMail@gmail.com</h3>
+              <h3>949 735 4092</h3>
+            </div>
+            <div className={styles.section}>
+              <h1>{t("footer.columnArticle.partners")}</h1>
+              <p>IMDB</p>
+              <p>Metarcritic</p>
+              <p>Rotten Tomatoes</p>
+            </div>
+          </div>
+          <div className={styles.socialsContainer}>
+            <h1>{t("footer.columnArticle.socials")}</h1>
+            <div className={styles.socialsIconsContainer}>
+              <FacebookIcon width={30} height={30} />
+              <InstagramIcon width={30} height={30} />
+              <TwitterIcon width={30} height={30} />
+              <TelegramIcon width={30} height={30} />
+            </div>
+          </div>
         </div>
-        <div className={styles.titlesContainer}>
-          <div className={styles.section}>
-            <h1>Legal</h1>
-            <p>Terms</p>
-            <p>Privacy</p>
-          </div>
-          <div className={styles.section}>
-            <h1>Explore</h1>
-            <p>Home</p>
-            <p>About</p>
-            <p>Careers</p>
-          </div>
-          <div className={styles.section}>
-            <h1>Get in touch</h1>
-            <h3>workMail@gmail.com</h3>
-            <h3>949 735 4092</h3>
-          </div>
-          <div className={styles.section}>
-            <h1>Partners</h1>
-            <p>IMDB</p>
-            <p>Metarcritic</p>
-            <p>Rotten Tomatoes</p>
-          </div>
-        </div>
-        <div className={styles.socialsContainer}>
-          <h1>Socials</h1>
-          <div className={styles.socialsIconsContainer}>
-            <FacebookIcon width={30} height={30} />
-            <InstagramIcon width={30} height={30} />
-            <TwitterIcon width={30} height={30} />
-            <TelegramIcon width={30} height={30} />
-          </div>
+        <div className={styles.changeLanguageContainer}>
+          <h3>{t("footer.languagePreset.chsLang")}: </h3>
+          <ul className={styles.languageList}>
+          {languages.map((lang) => (
+            <li onClick={() => {i18n.changeLanguage(lang); console.log(lang)}}>{t(`footer.languagePreset.${lang}`)}</li>
+          ))}
+          </ul>
         </div>
       </div>
     </div>
